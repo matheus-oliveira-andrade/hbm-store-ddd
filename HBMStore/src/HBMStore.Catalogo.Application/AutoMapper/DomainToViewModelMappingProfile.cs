e4c457ebbbebef78c82f3694d88a29a274b0a587
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-12-01 22:01:56 -0300

    added automapper, implementated application layer

---
+++ HBMStore/src/HBMStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HBMStore.Catalogo.Application.ViewModels;
+using HBMStore.Catalogo.Domain;
+
+namespace HBMStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, mo => mo.MapFrom(me => me.Dimensoes.Largura))
+                .ForMember(d => d.Altura, mo => mo.MapFrom(me => me.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, mo => mo.MapFrom(me => me.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ HBMStore/src/HBMStore.Catalogo.Application/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HBMStore.Catalogo.Application.ViewModels
+{
+    public class CategoriaViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do campo {0} é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do campo {0} é obrigatório")]
+        public int Codigo { get; set; }
+    }
+}
